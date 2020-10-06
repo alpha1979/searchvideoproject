a355b034339eb00ca023c83fec73349556d7411f
alpha1979 <atit1979@hotmail.com>
2020-10-06 14:51:55 +0100

    Updates

---
+++ precache-manifest.a73499488542d7ed384e138329492c63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d149233389daf338dc6c4fe477d1e84",
+    "url": "/searchvideoproject/index.html"
+  },
+  {
+    "revision": "141e53fa6df5d3625814",
+    "url": "/searchvideoproject/static/css/main.6e2c0122.chunk.css"
+  },
+  {
+    "revision": "b5f306dd8422e7e93942",
+    "url": "/searchvideoproject/static/js/2.4bae6257.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/searchvideoproject/static/js/2.4bae6257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "141e53fa6df5d3625814",
+    "url": "/searchvideoproject/static/js/main.2b804807.chunk.js"
+  },
+  {
+    "revision": "b80fd82ca5ed43e52530",
+    "url": "/searchvideoproject/static/js/runtime-main.e745919f.js"
+  }
+]);
